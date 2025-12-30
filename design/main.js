11e04fc137b7ae5e5c82ae21106e4b758b3a5cef
Peter Gerdzhikov <peter.gerdzhikov.contact@gmail.com>
2025-12-31 00:17:33 +0200

    Init repo with the login form html

---
+++ design/main.js
@@ -0,0 +1,8 @@
+const authenticationButton = document.querySelector(".submit-btn");
+
+authenticationButton.addEventListener("click", handleAuthenticationFormSubmit);
+
+
+function handleAuthenticationFormSubmit(e) {
+    console.log(e);
+}
